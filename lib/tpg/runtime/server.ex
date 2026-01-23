2917c0469c38a9a8cc6c9057c4d0a233c08e36d2
Joaquin H <hernandez.joaquin346@gmail.com>
2026-01-22 21:20:55 -0300

    added log info to server process actions

--- lib/tpg/runtime/server.ex
+++ lib/tpg/runtime/server.ex
@@ -1,5 +1,6 @@
 defmodule Tpg.Runtime.Server do
   use GenServer
+  require Logger
 
   def start_link(usuario) do
     GenServer.start_link(__MODULE__, usuario, name: {:global, usuario})
@@ -13,7 +14,13 @@ defmodule Tpg.Runtime.Server do
   def handle_call({:registrar_websocket, pid}, _from, state) do
     Process.monitor(pid)
     nuevos_ws = [pid | state.websocket_pids]
+    Logger.info("Websocket PID=#{inspect(pid)} asociado a Usuario=#{state.usuario}")
     {:reply, :ok, %{state | websocket_pids: nuevos_ws}}
+  end
+
+  def handle_call(:ver_historial, _from, state) do
+    mensajes_ordenados = Enum.reverse(state.mensajes)
+    {:reply, mensajes_ordenados, state}
   end
 
   def handle_cast({:recibir, de, mensaje}, state) do
@@ -27,19 +34,17 @@ defmodule Tpg.Runtime.Server do
 
     # Notificar a todos los WebSockets conectados
     Enum.each(state.websocket_pids, fn ws_pid ->
+      Logger.info("Notificando a WS PID=#{inspect(ws_pid)} asociado a Usuario=#{state.usuario}, el mensaje=#{mensaje}")
       send(ws_pid, {:nuevo_mensaje, de, mensaje, nuevo_mensaje.timestamp})
     end)
 
     {:noreply, %{state | mensajes: nuevos_mensajes}}
   end
 
-  def handle_call(:ver_historial, _from, state) do
-    mensajes_ordenados = Enum.reverse(state.mensajes)
-    {:reply, mensajes_ordenados, state}
-  end
 
   # Limpiar WebSockets caídos
   def handle_info({:DOWN, _ref, :process, pid, _reason}, state) do
+    Logger.info("WebSocket PID=#{inspect(pid)} desconectado de Usuario=#{state.usuario}")
     nuevos_ws = List.delete(state.websocket_pids, pid)
     {:noreply, %{state | websocket_pids: nuevos_ws}}
   end
