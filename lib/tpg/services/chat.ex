3f4a77f456495d618c9ca8a38cf4e151a504a352
Joaquin H <hernandez.joaquin346@gmail.com>
2026-01-23 00:29:24 -0300

    intento de guardar mensaje en db

--- lib/tpg/services/chat.ex
+++ lib/tpg/services/chat.ex
@@ -7,13 +7,14 @@ defmodule Tpg.Services.Chat do
   end
 
   def agregar_mensaje(chat, de, contenido) do
-    nuevo_msg = %{emisor: de, contenido: contenido, fecha: Time.utc_now()}
+    nuevo_msg = %{emisor: de, contenido: contenido, estado: "ENVIADO", fecha: Time.utc_now()}
     Logger.info("Guardando mensaje...: #{nuevo_msg.contenido}, de #{de}")
-    case Tpg.Mensajes.MultiInsert.enviar_mensaje( chat.usuario, de, contenido ) do
+    case Tpg.Mensajes.MultiInsert.enviar_mensaje( chat.usuario, de, nuevo_msg ) do
       {:ok, mensaje} ->
         Logger.info("Mensaje guardado: #{nuevo_msg.contenido}, de #{de}")
         {:ok, %{chat | mensajes: [nuevo_msg | chat.mensajes]}}
       {:error, motivo} ->
+        Logger.alert("Mensaje perdido: #{nuevo_msg.contenido}, de #{de}. Motivo: #{inspect(motivo)}")
         {:error, motivo}
     end
   end
